1a88353f3b8e6e7fadf172ee4f1a87bb7db7bf6e
julakhss <144143635+julakhss@users.noreply.github.com>
2025-08-26 17:14:05 +0800

    Update xolpanel.sh

--- xolpanel.sh
+++ xolpanel.sh
@@ -3,7 +3,7 @@
 #install
 apt update && apt upgrade
 apt install python3 python3-pip git
-git clone https://github.com/hokagelegend9999/bot_panel.git
+git clone https://github.com/julakhss/bot_panel.git
 unzip bot_panel/xolpanel.zip
 pip3 install -r xolpanel/requirements.txt
 pip3 install pillow
